90908c09e44d936da1e78042b57d55af3ae2f1bd
DhruvPopat268 <dhruvpopat268@gmail.com>
2025-07-30 16:23:28 +0530

    updated code

--- backend/server.js
+++ backend/server.js
@@ -19,8 +19,8 @@ const server = http.createServer(app);
 const allowedOrigins = [
   "http://localhost:3000",
   "https://chat-app-prayosha.vercel.app/",
-  "https://chat-app-prayosha-git-master-dhruvs-projects-cffe63d8.vercel.app/",
-  "https://chat-app-prayosha-hl15k3spo-dhruvs-projects-cffe63d8.vercel.app/"
+  "https://chat-app-prayosha-git-master-dhruvs-projects-cffe63d8.vercel.app",
+  "https://chat-app-prayosha-hl15k3spo-dhruvs-projects-cffe63d8.vercel.app"
 ];
 
 app.use(cors({
